1cf319d20d72a69c3a6d9f77136732c01f71593f
Zorin Aleksey <realza657@gmail.com>
2025-03-17 15:28:03 +1000

    FirstThreePages update

---
+++ app/src/main/java/com/example/learning2/Registration.kt
@@ -0,0 +1,11 @@
+package com.example.learning2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Registration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
